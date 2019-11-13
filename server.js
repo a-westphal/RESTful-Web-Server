7d7e8006070536e702a7f755ae6d60bed9487c20
Abby Westphal <47372310+a-westphal@users.noreply.github.com>
2019-11-13 15:07:25 -0600

    added promises to database pulls

--- server.js
+++ server.js
@@ -32,88 +32,99 @@ var incidents = new Object();
 //GET /codes
 app.get('/codes',(req,res) =>{
 	//create a javascript object to store the codes: 
-	db.all('SELECT * FROM Codes ORDER BY code',(err,rows)=>{
-		rows.forEach(function (row){
-			//add the code as a new key and the incident type as a new value 
-			codes[row.code] = row.incident_type;
-		})
+	var database_Promise = new Promise ((resolve,reject)=>{
+		db.all('SELECT * FROM Codes ORDER BY code',(err,rows)=>{
+			rows.forEach(function (row){
+				//add the code as a new key and the incident type as a new value 
+				codes[row.code] = row.incident_type;
+			})
 
-	});
+		});
+
+	})
+	database_Promise.then(data=>{
+
+	})
 
 }); 
 
 //GET /neighborhoods
 app.get('/neighborhoods',(req,res)=>{
 	//create a javascript object to store the neighborhood numbers and names 
-	db.all('SELECT * FROM Neighborhoods ORDER BY neighborhood_number',(err,rows)=>{
+	var database_Promise = new Promise ((resolve,reject) =>{
+		db.all('SELECT * FROM Neighborhoods ORDER BY neighborhood_number',(err,rows)=>{
+			rows.forEach(function(row){
+				//add the neighborhood number as a new key and the neighborhood name as a new value 
+				neighborhoods[row.neighborhood_number] = row.neighborhood_name;
+			})
 
-		rows.forEach(function(row){
-			//add the neighborhood number as a new key and the neighborhood name as a new value 
-			neighborhoods[row.neighborhood_number] = row.neighborhood_name;
-		})
+		});
 
-	});
+	})
 
 });
 
 //GET /incidents
 app.get('/incidents',(req,res)=>{
 	//we want the most recent incident number first: 
-	db.all('SELECT * FROM Incidents ORDER BY case_number DESC',(err,rows)=>{
-		rows.forEach(function(row){
-			//for each case number, a new Object 
-			incidents[case_number] = new Object();
-			incidents[case_number][date_time] = row.date_time;
-			incidents[case_number][code] = row.code;
-			incidents[case_number][incident] = row.incident;
-			incidents[case_number][police_grid] = row.police_grid;
-			incidents[case_number][neighborhood_number] = row.neighborhood_number;
-			incidents[case_number][block] = row.block;
-		})
+	var database_Promise = new Promise ((resolve,reject) =>{
+		db.all('SELECT * FROM Incidents ORDER BY case_number DESC',(err,rows)=>{
+			rows.forEach(function(row){
+				//for each case number, a new Object 
+				incidents[case_number] = new Object();
+				incidents[case_number][date_time] = row.date_time;
+				incidents[case_number][code] = row.code;
+				incidents[case_number][incident] = row.incident;
+				incidents[case_number][police_grid] = row.police_grid;
+				incidents[case_number][neighborhood_number] = row.neighborhood_number;
+				incidents[case_number][block] = row.block;
+			})
 
-
-	});
+		});
+	})
 
 });
 
 //PUT /new-incident
 app.put('new-incident',(req,res)=>{
 	var incidents = new Object();
-	db.all('SELECT * FROM Incidents ORDER BY case_number DESC',(err,rows)=>{
-		rows.forEach(function(row){
-			//for each case number, a new Object 
-			incidents[case_number] = new Object();
-			incidents[case_number][date_time] = row.date_time;
-			incidents[case_number][code] = row.code;
-			incidents[case_number][incident] = row.incident;
-			incidents[case_number][police_grid] = row.police_grid;
-			incidents[case_number][neighborhood_number] = row.neighborhood_number;
-			incidents[case_number][block] = row.block;
-		})
+	var database_Promise = new Promise((resolve,reject)=>{
+		db.all('SELECT * FROM Incidents ORDER BY case_number DESC',(err,rows)=>{
+			rows.forEach(function(row){
+				//for each case number, a new Object 
+				incidents[case_number] = new Object();
+				incidents[case_number][date_time] = row.date_time;
+				incidents[case_number][code] = row.code;
+				incidents[case_number][incident] = row.incident;
+				incidents[case_number][police_grid] = row.police_grid;
+				incidents[case_number][neighborhood_number] = row.neighborhood_number;
+				incidents[case_number][block] = row.block;
+			})
 
-	});
+		});
+	})
+	database_Promise.then(data=>{
+		var new_incident = new Object();
+		new_casenum = parseInt(req.body.case_number,10)
+		new_incident[new_casenum] = new Object();
+		new_incident[new_casenum][date_time] = req.body.date_time;
+		new_incident[new_casenum][code] = parseInt(req.body.code,10);
+		new_incident[new_casenum][incident] = req.body.incident;
+		new_incident[new_casenum][police_grid] = parseInt(req.body.police_grid,10);
+		new_incident[new_casenum][neighborhood_number]=parseInt(req.body.neighborhood_number,10);
+		new_incident[new_casenum][block] = req.body.block;
 
-	var new_incident = new Object();
-	new_casenum = parseInt(req.body.case_number,10)
-	new_incident[new_casenum] = new Object();
-	new_incident[new_casenum][date_time] = req.body.date_time;
-	new_incident[new_casenum][code] = parseInt(req.body.code,10);
-	new_incident[new_casenum][incident] = req.body.incident;
-	new_incident[new_casenum][police_grid] = parseInt(req.body.police_grid,10);
-	new_incident[new_casenum][neighborhood_number]=parseInt(req.body.neighborhood_number,10);
-	new_incident[new_casenum][block] = req.body.block;
-
-	//add the new incident into the incident object
-	incidents.push(new_incident);
-
-/*	db.run(`INSERT INTO Incidents(new_incident) VALUES(?)`,function(err){
+		//add the new incident into the incident object
+		incidents.push(new_incident);
+		/*	db.run(`INSERT INTO Incidents(new_incident) VALUES(?)`,function(err){
 		if(err){
 	
 		}
 
-	})
+		})
 
-*/ 
+		*/ 
+	})
 });
 
 var server = app.listen(port);
