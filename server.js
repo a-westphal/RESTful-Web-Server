56d431a3c5ffd2f8ae01de628906a1abc1e4b176
a-westphal <west8659@stthomas.edu>
2019-11-17 20:18:03 -0600

    Significant progress with extra commands

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 // NPM modules
 var express = require('express');
 var sqlite3 = require('sqlite3');
-//var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 
 
 var public_dir = path.join(__dirname, 'public');
@@ -26,6 +26,8 @@ var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
 });
 
 app.use(express.static(public_dir));
+app.use(bodyParser.urlencoded({extended: true}));
+
 var codes = new Object();
 var neighborhoods = new Object();
 var incidents = new Object();
@@ -33,11 +35,35 @@ var incidents = new Object();
 //GET /codes
 app.get('/codes',(req,res) =>{
 	//create a javascript object to store the codes: 
+	//console.log(req.query.code);
+
+	var url =req.url;
+
 	var database_Promise = new Promise ((resolve,reject)=>{
 		db.all('SELECT * FROM Codes ORDER BY code',(err,rows)=>{
 			rows.forEach(function (row){
 				//add the code as a new key and the incident type as a new value 
-				codes[row.code] = row.incident_type;
+				let add = "C";
+
+				if(url.length > 6 && req.query.hasOwnProperty("code")){
+						
+					var select_code =  req.query.code.split(',');
+					console.log("selected codes:" + select_code);
+					console.log("db row: " +row.code);
+					for(let i =0; i < select_code.length; i ++)
+					{
+						if(row.code == select_code[i])
+						{
+							console.log("row code is:" + row.code);
+							codes[add.concat("",select_code[i])] = row.incident_type;
+						}
+					}
+				}
+
+				else{
+					codes[add.concat("",row.code)] = row.incident_type;
+				}
+
 			})
 
 			resolve(codes);
@@ -47,7 +73,19 @@ app.get('/codes',(req,res) =>{
 	})
 	database_Promise.then(data=>{
 		//check if the query string specifices the format type here (to be added later)
-		res.type('json').send(codes);
+		let formatter = "json";
+
+		if(req.query.hasOwnProperty("format"))
+		{
+			//to be edited with the xml stuff
+			console.log("if statement");
+			//edit something to xml 
+		}
+		else{
+			res.type('json').send(codes);
+		} 
+
+		//res.type('json').send(codes);
 	})
 
 }); 
@@ -55,11 +93,33 @@ app.get('/codes',(req,res) =>{
 //GET /neighborhoods
 app.get('/neighborhoods',(req,res)=>{
 	//create a javascript object to store the neighborhood numbers and names 
+	var url = req.url;
+	console.log(url.length);
 	var database_Promise = new Promise ((resolve,reject) =>{
 		db.all('SELECT * FROM Neighborhoods ORDER BY neighborhood_number',(err,rows)=>{
 			rows.forEach(function(row){
 				//add the neighborhood number as a new key and the neighborhood name as a new value 
-				neighborhoods[row.neighborhood_number] = row.neighborhood_name;
+				let add = "N";
+
+				if(url.length > 14 && req.query.hasOwnProperty("id")){
+						
+					var select_id =  req.query.id.split(',');
+					//console.log("selected codes:" + select_id);
+					//console.log("db row: " +row.neighborhood_number);
+					for(let i =0; i < select_id.length; i ++)
+					{
+						if(row.neighborhood_number == select_id[i])
+						{
+							//console.log("row id is:" + row.neighborhood_number);
+							neighborhoods[add.concat("",select_id[i])] = row.neighborhood_name;
+						}
+					}
+				}
+
+				else{
+					neighborhoods[add.concat("",row.neighborhood_number)] = row.neighborhood_name;
+				}
+				
 			})
 
 			resolve(neighborhoods);
@@ -68,7 +128,18 @@ app.get('/neighborhoods',(req,res)=>{
 
 	database_Promise.then(data => {
 		//check if the query string specifies the format type here (to be added later)
-		res.type('json').send(neighborhoods);
+			let formatter = "json";
+
+		if(req.query.hasOwnProperty("format"))
+		{
+			//to be edited with the xml stuff
+			console.log("if statement");
+			//edit something to xml 
+		}
+		else{
+			res.type('json').send(neighborhoods);
+		} 
+
 	})
 
 });
@@ -76,18 +147,78 @@ app.get('/neighborhoods',(req,res)=>{
 //GET /incidents
 app.get('/incidents',(req,res)=>{
 	//we want the most recent incident number first: 
+
+	var url = req.url;
+	console.log(url.length);
 	var database_Promise = new Promise ((resolve,reject) =>{
 		db.all('SELECT * FROM Incidents ORDER BY case_number DESC',(err,rows)=>{
 			rows.forEach(function(row){
-				//for each case number, a new Object 
-				let case_number = row.case_number;
-				incidents[case_number] = new Object();
-				incidents[case_number]["date_time"] = row.date_time;
-				incidents[case_number]["code"] = row.code;
-				incidents[case_number]["incident"] = row.incident;
-				incidents[case_number]["police_grid"] = row.police_grid;
-				incidents[case_number]["neighborhood_number"] = row.neighborhood_number;
-				incidents[case_number]["block"] = row.block;
+				//for each case number, a new Object
+				let add = "I"; 
+				let case_number = add.concat("",row.case_number);
+
+				if(url.length > 10 && req.query.hasOwnProperty("id")){
+						
+					var select_id =  req.query.id.split(',');
+					//console.log("selected codes:" + select_id);
+					//console.log("db row: " +row.neighborhood_number);
+					for(let i =0; i < select_id.length; i ++)
+					{
+						if(row.neighborhood_number == select_id[i])
+						{
+							//console.log("row id is:" + row.neighborhood_number);
+							incidents[case_number] = new Object();
+							incidents[case_number]["date"] = date_time[0];
+							incidents[case_number]["time"] = date_time[1];
+							incidents[case_number]["code"] = row.code;
+							incidents[case_number]["incident"] = row.incident;
+							incidents[case_number]["police_grid"] = row.police_grid;
+							incidents[case_number]["neighborhood_number"] = select_id[i];
+							incidents[case_number]["block"] = row.block;
+						}
+					}
+				}
+
+				else if(url.length > 10 && req.query.hasOwnProperty("grid"))
+				{
+					var select_grid = req.query.grid.split(',');
+					for(let i =0; i < select_id.length; i ++)
+					{
+						if(row.police_grid == select_grid[i])
+						{
+							incidents[case_number] = new Object();
+							incidents[case_number]["date"] = date_time[0];
+							incidents[case_number]["time"] = date_time[1];
+							incidents[case_number]["code"] = row.code;
+							incidents[case_number]["incident"] = row.incident;
+							incidents[case_number]["police_grid"] = select_grid[i];
+							incidents[case_number]["neighborhood_number"] = row.neighborhood_number;
+							incidents[case_number]["block"] = row.block;
+						}
+					}
+
+				}
+
+				else if(url.length > 10 && req.query.hasOwnProperty("code"))
+				{
+
+				}
+
+				//need code, start and end date, limit check
+
+				else{
+					let date_time = row.date_time.split("T");
+					incidents[case_number] = new Object();
+					incidents[case_number]["date"] = date_time[0];
+					incidents[case_number]["time"] = date_time[1];
+					incidents[case_number]["code"] = row.code;
+					incidents[case_number]["incident"] = row.incident;
+					incidents[case_number]["police_grid"] = row.police_grid;
+					incidents[case_number]["neighborhood_number"] = row.neighborhood_number;
+					incidents[case_number]["block"] = row.block;
+				}
+
+				
 			})
 
 			resolve(incidents);
